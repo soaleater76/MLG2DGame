19766f40b0f6972bde79b98b370c86469270eca2
soaleater76 <soaleater76@gmail.com>
2015-03-12 16:46:20 +0000

    Added not working spawner script

---
+++ Amazing2DGame/Amazing 2D Game/Assets/Scripts/Spawner.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var GroundPrefab : GameObject;
+
+function Start () 
+{
+	SpawnPrefabs();
+}
+
+function Update () 
+{
+	
+}
+
+function SpawnPrefabs()
+{
+	Instantiate(GroundPrefab, transform.position, Quaternion.identity);
+	WaitForSeconds(1);
+	SpawnPrefabs();
+}
